f89cb8b307e7b1eebc10fe224a351216e2a2e49c
Rafał Nowak <rafal.nowak@tooploox.com>
2022-04-20 21:47:51 +0200

    added loading from checkpoint

--- examples/train_mnist.py
+++ examples/train_mnist.py
@@ -1,14 +1,19 @@
 import pytorch_lightning as pl
 from pl_wandb_demo.data_modules import MNISTDataModule
 from pl_wandb_demo.models import MNISTClassifier
+import argparse
 
 
 def main():
-    dm = MNISTDataModule(batch_size=32)
-    model = MNISTClassifier(units=100)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", type=str, required=True)
+    args = parser.parse_args()
 
-    trainer = pl.Trainer(gpus=-1, max_epochs=10)
-    trainer.fit(model, dm)
+    dm = MNISTDataModule(batch_size=32)
+    model = MNISTClassifier.load_from_checkpoint(checkpoint_path=args.model_path)
+
+    trainer = pl.Trainer(gpus=-1, max_epochs=25)
+    trainer.fit(model, datamodule=dm, ckpt_path=args.model_path)
 
 
 if __name__ == "__main__":
