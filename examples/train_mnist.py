cc519fcda1ccfc5c3107f720eea63c2889ac494f
Rafał Nowak <rafal.nowak@tooploox.com>
2022-04-20 22:52:23 +0200

    added Rich*Calbacks

--- examples/train_mnist.py
+++ examples/train_mnist.py
@@ -1,10 +1,28 @@
 import argparse
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, RichModelSummary, RichProgressBar
 
 from pl_wandb_demo.data_modules import MNISTDataModule
 from pl_wandb_demo.models import MNISTClassifier
 from pl_wandb_demo.utils import load_config
+
+
+def get_callbacks():
+
+    model_checkpoint_callback = ModelCheckpoint(
+        dirpath="data/checkpoints",
+        filename="mnist-epoch{epoch:02d}-val_loss{val/loss:.2f}",
+        monitor="val/loss", mode="min",
+        auto_insert_metric_name=False,
+        verbose=True
+    )
+
+    return [
+        model_checkpoint_callback,
+        RichModelSummary(),
+        RichProgressBar()
+    ]
 
 
 def main():
@@ -17,10 +35,11 @@ def main():
     dm = MNISTDataModule(config=config["data_module_config"])
     model = MNISTClassifier(config=config["model_config"])
 
-    trainer = pl.Trainer(**config["trainer_args"])
+    callbacks = get_callbacks()
+
+    trainer = pl.Trainer(**config["trainer_args"], callbacks=callbacks)
     trainer.fit(model, datamodule=dm, ckpt_path=args.checkpoint)
 
 
 if __name__ == "__main__":
     main()
-
